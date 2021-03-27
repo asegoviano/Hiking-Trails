0994b396b8bd1ddc694f3bc293d4c5db67f7a3ef
Angel Segoviano <37810476+asegoviano@users.noreply.github.com>
2021-03-27 16:30:29 -0700

    updated Homepage css

--- hikingtrailsreact/src/components/HomePage.js
+++ hikingtrailsreact/src/components/HomePage.js
@@ -5,19 +5,48 @@
  * js page for homePage
  */
 import React from 'react'
+import {Carousel, Container, Row, Col, Image} from 'react-bootstrap';
 import Navbar from "./Navbar";
-//import Trail from '../images/trail3.jpg';
+import trail1 from '../images/trail1.jpg';
+import trail2 from '../images/trail2.jpg';
+import trail3 from '../images/trail3.jpg';
+import trail4 from '../images/trail4.jpg';
+import bookmarkIcon from '../images/bookmarkIcon.png';
+import searchIcon from '../images/searchIcon.png';
 
 class HomePage extends React.Component {
     render() {
         return (
             <div>
-                {/* displays the content from the navbar component */}
                 <Navbar/>
-                Welcome To The Home Page
-               id; {this.props.dataFormParent.passUserId}
-                role:{this.props.dataFormParent.role}
-                {/* <img className="trail" src={Trail}/> */}
+                    <h1>Welcome Page</h1>
+                    <Container>
+                        <Row className="rowCSSC">
+                        <Carousel interval={2000} keyboard={false} pauseOnHover={false}>
+                            <Carousel.Item style={{'height':"400px", 'width':"1000px"}} >
+                                <img style={{'height':"400px", 'width':"1000px"}} className="d-block w-100" src={trail1} alt="Trail"/>
+                            </Carousel.Item>
+                            <Carousel.Item style={{'height':"400px", 'width':"1000px"}} >
+                                <img style={{'height':"400px", 'width':"1000px"}} className="d-block w-100" src={trail2} alt="Trail"/>
+                            </Carousel.Item>
+                            <Carousel.Item style={{'height':"400px", 'width':"1000px"}} >
+                                <img style={{'height':"400px", 'width':"1000px"}} className="d-block w-100" src={trail3} alt="Trail" />
+                            </Carousel.Item>
+                            <Carousel.Item style={{'height':"400px", 'width':"1000px"}} >
+                                <img style={{'height':"400px", 'width':"1000px"}} className="d-block w-100" src={trail4} alt="Trail" />
+                            </Carousel.Item>
+                        </Carousel>
+                    </Row>
+                    <Row className="rowCSS">
+                        <Col xs={6} md={4}>
+                            <Image className="bookmarkIcon" src={bookmarkIcon} alt="bookmarkIcon" roundedCircle/>
+                            <text>testing </text>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Image className="searchIcon" src={searchIcon} alt="searchIcon" roundedCircle/>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
