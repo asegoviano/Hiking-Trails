57377a9f7c0de3e68b2123b29837c2ff630a01e2
Angel Segoviano <37810476+asegoviano@users.noreply.github.com>
2021-04-03 19:08:09 -0700

    updated Navbar

--- hikingtrailsreact/src/components/Navbar.js
+++ hikingtrailsreact/src/components/Navbar.js
@@ -13,27 +13,24 @@ class Navbar extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            
         }
     }
+
     render() {
         return (
             <div>
                 <header>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <span className="navbar-brand" href="#">HikingTrails</span>
+                        <span className="navbar-brand" href="#" ><Link to="/home">HikingTrails</Link></span>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <span className="nav-link" href="#"><Link to="/home">Home</Link></span>
+                        <div className="navbar-nav" >
                             <span className="nav-link" href="#"><Link to="/profile">Profile</Link></span>
                             <span className="nav-link" href="#"><Link to="/explore">Exlpore</Link></span>
-                            <span className="nav-link" href="#"><Link to="/bookmark">My Bookmarks</Link></span>
-                            <span className="nav-link" href="#"><Link to="/event">My Events</Link></span>
-                            <span className="nav-link" href="#"><Link to="/admin">Admin</Link></span>
+                            {/* {dataFormParent==='Admin' ? <span className="nav-link" href="#"><Link to="/admin">Admin</Link></span> : <span/>} */}
                             <span className="nav-link" href="#"><Link to="/">Logout</Link></span>
                         </div>
                         </div>
