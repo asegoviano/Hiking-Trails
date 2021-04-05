dd8e3948dcdbf6b4a42cb11e808bb789e173ee7c
Angel Segoviano <37810476+asegoviano@users.noreply.github.com>
2021-04-04 18:25:26 -0700

    Update HomePage.js
    worked on css

--- hikingtrailsreact/src/components/HomePage.js
+++ hikingtrailsreact/src/components/HomePage.js
@@ -5,7 +5,7 @@
  * js page for homePage
  */
 import React from 'react'
-import {Carousel, Container, Row, Col, Image} from 'react-bootstrap';
+import { Container, Row, Col, Image} from 'react-bootstrap';
 import Navbar from "./Navbar";
 import bookmarkIcon from '../images/bookmarkIcon.png';
 import searchIcon from '../images/searchIcon.png';
@@ -21,22 +21,18 @@ class HomePage extends React.Component {
                                 <img style={{'height':"425px", 'width':"400px"}} className="d-block w-100" src={tr1} alt="Trail"/>
                         </Row>
                     <Row className="rowCSS">
-                        <div className="logoBox">
                             <Col xs={6} md={4}>
                                 <Image className="bookmarkIcon" src={bookmarkIcon} alt="bookmarkIcon" height='100px' width='120px'/>
                                 <Row>
-                                    <text>Go the explore page to bookmark a trail and keep track of which ones you like the most in your profile.</text>
+                                    <text>Check out your favorites by adding a trail to your favorites from the Explore Page!</text>
                                 </Row>
                             </Col>
-                        </div>
-                        <div>
                             <Col xs={6} md={4}>
                                 <Image className="searchIcon" src={searchIcon} alt="searchIcon" roundedCircle/>
                                 <Row>
-                                    <text>testing search icon description </text>
+                                    <text>Go to the Explore Page and search through a list of trails and see which one looks fun!</text>
                                 </Row>
                             </Col>
-                        </div>
                     </Row>
                 </Container>
             </div>
